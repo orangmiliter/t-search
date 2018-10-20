9abe11a074101284c1eab12691fe7d5df3ffdc42
orangmiliter <fazlurbima@gmail.com>
2018-10-21 01:02:16 +0700

    add iframe

--- Tsearch.py
+++ Tsearch.py
@@ -6,17 +6,17 @@ from googlesearch import search
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--target", help="Target")
+parser.add_argument("--target", help="Target", nargs='+')
 args = parser.parse_args()
-
-keyword = str(args.target)
+argspace = ' '.join(args.target)
+keyword = str(argspace)
 
 ganti = "%s" % keyword
 hasil = ganti.replace(" ", "_")
 
 os.mkdir("output/%s" % hasil)
 
-for url in search("'%s' site:t.me" % str(keyword), stop=150):
+for url in search("'%s' site:t.me" % str(keyword), stop=10):
     file = open("output/%s.txt" % str(keyword), 'a')
     file.write("%s\n" % url)
     file.close()
@@ -25,5 +25,17 @@ for url in search("'%s' site:t.me" % str(keyword), stop=150):
 #createScreenshot
 lis = [x.rstrip() for x in open('output/%s.txt' % str(keyword), 'r').readlines() if len(x.rstrip())]
 for link in lis:
-   rndm = random.random()
-   os.system("wget -nH --cut-dirs=1 --output-document=output/%s/%s.html --convert-links %s" % (hasil, str(rndm), link))
+	rndm = random.random()
+	os.system("wget -nH --cut-dirs=1 --output-document=output/%s/%s.html --convert-links %s" % (hasil, str(rndm), link))
+	os.system("ls output/%s/ > output/%s.txt" % (hasil, hasil))
+
+#Createiframe
+lis = [x.rstrip() for x in open('output/%s.txt' % hasil, 'r').readlines() if len(x.rstrip())]
+for frame in lis:
+	fIframe = open('output/%s.html' % hasil, 'a')
+	Iframe = """
+	<iframe src="%s/%s" height="200" width="300" frameborder="3" gesture="media" allow="encrypted-media" allowfullscreen>></iframe>""" % (hasil, frame)
+	fIframe.write(Iframe)
+	fIframe.close()
+
+os.remove('output/%s.txt' % hasil)
